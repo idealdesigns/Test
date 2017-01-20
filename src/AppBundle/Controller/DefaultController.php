c6e00270818207f2a061ee9c35bf28afbd699f1d
Hussein Abdul Kader <idealdesigns.h@gmail.com>
2017-01-20 19:01:44 +0100

    in progress validate soduko

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,7 +16,139 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
         ]);
+    }
+
+
+    /**
+     * @Route("/print", name="print")
+     */
+    public function printSudoku(Request $request)
+    {
+        $jsonBoard = '
+	[{
+			"0": 7,
+			"1": null,
+			"2": null,
+			"3": null,
+			"4": 4,
+			"5": null,
+			"6": 5,
+			"7": 3,
+			"8": null
+		},
+
+		{
+			"0": 7,
+			"1": null,
+			"2": 5,
+			"3": null,
+			"4": null,
+			"5": 8,
+			"6": null,
+			"7": 1,
+			"8": null
+		}, {
+			"0": null,
+			"1": null,
+			"2": 8,
+			"3": 5,
+			"4": null,
+			"5": 9,
+			"6": null,
+			"7": 4,
+			"8": null
+		}, {
+			"0": 5,
+			"1": 3,
+			"2": 9,
+			"3": null,
+			"4": 6,
+			"5": null,
+			"6": null,
+			"7": null,
+			"8": 1
+		}, {
+			"0": null,
+			"1": null,
+			"2": null,
+			"3": null,
+			"4": 1,
+			"5": null,
+			"6": null,
+			"7": null,
+			"8": 5
+		},
+
+		{
+			"0": 8,
+			"1": null,
+			"2": null,
+			"3": 7,
+			"4": 2,
+			"5": null,
+			"6": 9,
+			"7": null,
+			"8": null
+		},
+
+		{
+			"0": 9,
+			"1": null,
+			"2": 7,
+			"3": 4,
+			"4": null,
+			"5": null,
+			"6": null,
+			"7": null,
+			"8": null
+		},
+
+		{
+			"0": null,
+			"1": null,
+			"2": null,
+			"3": null,
+			"4": 5,
+			"5": 7,
+			"6": null,
+			"7": null,
+			"8": null
+		},
+
+		{
+			"0": 6,
+			"1": null,
+			"2": null,
+			"3": null,
+			"4": null,
+			"5": null,
+			"6": null,
+			"7": 5,
+			"8": null
+		}]
+';
+
+
+        $sudoku = json_decode($jsonBoard,true);
+
+        // printing the formated input sudoku
+        $output = "<B>Input Sudoku:</B><br />";
+        $output.="<table border='1'>";
+        foreach ($sudoku as $row) {
+            $output .= "<tr>";
+            foreach ($row as $col) {
+                $output .=  ("<td>" . $col . "</td>");
+            }
+            $output .= "</tr>";
+        }
+
+        $output.="</table>";
+        $output .= "<hr />";
+
+        print_r($output);exit;
+
+        return new Response($output);
     }
 }
--- src/AppBundle/Controller/SodukoController.php
+++ src/AppBundle/Controller/SodukoController.php
@@ -4,8 +4,10 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 
@@ -17,7 +19,7 @@ class SodukoController extends Controller
      * @Route("/validate",name="validate")
      * @Method({"POST"})
      * @param Request $request
-    */
+     */
     public function validateSoduku(Request $request)
     {
         $board = $request->request->get("board");
@@ -32,7 +34,31 @@ class SodukoController extends Controller
         $columns = array();
         $cubes = array();
 
+        // process each cell only once
+        for ($row = 0; $row < 9; ++$row) {
+            for ($column = 0; $column < 9; ++$column) {
+                $current = $board[$row][$column];
+                if (is_integer($current) || is_null($current)) {
+                    // determine which of the "cubes" the row/col fall in
+                    $cube = 3 * ($row / 3) + ($column / 3);
 
+                    if ((isset($rows[$row]) && $rows[$row] == $current) ||
+                        (isset($columns[$column]) && $columns[$column] == $current) ||
+                        (isset($cubes[$cube]) && $cubes[$cube] == $current)
+                    ) {
+                        throw new HttpException(400, "Invalid Soduko Board");
+                    }
+
+
+                } else {
+                    throw new HttpException(400, "Board contains invalid character: $current");
+                }
+            }
+
+
+        }
+
+        return new JsonResponse(null,204);
     }
 }
 
