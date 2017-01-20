b1c21f9ea02f7ef809f47f01296e1b51875d1100
Hussein Abdul Kader <idealdesigns.h@gmail.com>
2017-01-20 16:59:22 +0100

    in progress validate soduko board

--- src/AppBundle/Controller/SodukoController.php
+++ src/AppBundle/Controller/SodukoController.php
@@ -6,17 +6,33 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 
 class SodukoController extends Controller
 {
 
+
     /**
      * @Route("/validate",name="validate")
-     * @Method({"GET"})
-     * @param Reruest $rerquest
-     */
-    public function validateSoduku(Request $rerquest){
-        die("ae");
+     * @Method({"POST"})
+     * @param Request $request
+    */
+    public function validateSoduku(Request $request)
+    {
+        $board = $request->request->get("board");
+
+
+        //Validate if the board is 9x9 array
+        if ($board == null || count($board[0]) != 9 || count($board[1]) != 9) {
+            throw new HttpException(400, "Board is not valid size for Sudoku.");
+        }
+
+        $rows = array();
+        $columns = array();
+        $cubes = array();
+
+
     }
 }
+
